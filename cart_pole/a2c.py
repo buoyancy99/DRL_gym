58f1e8bd68171ae47ffdab521e161483bf5a5588
Boyuan Chen <boyuanchen@berkeley.edu>
2019-05-30 13:12:28 -0700

    implement methods and main code, update utils neral nets for a2c

--- cart_pole/a2c.py
+++ cart_pole/a2c.py
@@ -1,20 +1,63 @@
-from .utils import fc_net
+"""
+#################
+This piece of code referenced
+https://github.com/rlcode/reinforcement-learning
+but also has 
+#################
+"""
+from .utils import actor_net, critic_net
 from collections import deque
 import numpy as np
+import torch
+import torch.nn as nn
+
+episodes = 1000 
+render = True
+
 
 class A2C_Agent():
-	def __init__(self):
-		pass
+	def __init__(self, state_size, action_size):
+		self.state_size = state_size
+		self.action_size = action_size
+		self.actor = fc_net(state_size, action_size)
+		self.critic = fc_net(state_size, 1)
+		self.actor_lr = 0.001
+		self.critic_lr = 0.005
+		self.gamma = 0.99
 
-	def get_action(self):
-		pass
 
-	def regress(self):
-		pass
+	def get_action(self, state):
+		policy = self.actor(state.view(1, state_size))[0]
+		return np.random.choice(self.action_size, p = policy)
 
-
-
+	def regress(state, action, reward, next_state, done):
+		V_state = self.critic(state)
+		V_next_state = self.actor)_
+	
+		if done:
+			pass
+		else:
+			advantage = reward + 
 
 
 if __name__ == "__main__":
-	pass
+	env = gym.make('CartPole-v1')
+	state_size = env.observation_space.shape[0]
+	action_size = env.action_space.n
+
+	agent = A2C_Agent(state_size, action_size)
+	
+	for e in range(episodes):
+		done = False
+		state = env.reset().reshape((1, state_size))
+
+		while not done:
+			if render:
+				env.render()
+
+			action = agent.get_cation(state)
+			next_state, reward, donem, info = env.step(action)
+			next_state = next_state.reshape((1, state))
+			agent.regress(state, action, reward, next_state, done)
+
+		
--- cart_pole/utils.py
+++ cart_pole/utils.py
@@ -6,7 +6,31 @@ class fc_net(nn.Module):
 		self.layers = nn.Sequential(
 			nn.Linear(in_C, 32),
 			nn.ReLU(),
-			nn.Linear(32, 32),
+			nn.Linear(32, out_C)
+		)
+
+	def forward(self, X):
+		return self.layers(X)
+
+class actor_net(nn.Module):
+	def __init__(self, in_C, out_C):
+		super(actor_net, self).__init__()
+		self.layers = nn.Sequential(
+			nn.Linear(in_C, 32),
+			nn.ReLU(),
+			nn.Linear(32, out_C)
+			nn.Softmax()
+		)
+
+	def forward(self, X):
+		return self.layers(X)
+
+
+class critic_net(nn.Module):
+	def __init__(self, in_C, out_C):
+		super(critic_net, self).__init__()
+		self.layers = nn.Sequential(
+			nn.Linear(in_C, 32),
 			nn.ReLU(),
 			nn.Linear(32, out_C)
 		)
